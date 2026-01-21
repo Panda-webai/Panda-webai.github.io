ed50a01d8e2998b9d94ddb9aa9cb694470b89f6a
Panda-webai <viraput.c@gmail.com>
2026-01-21 14:11:44 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,9 +118,9 @@ function showProjects(projects) {
         <div class="desc">
           <p>${project.desc}</p>
           <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
+            <a href="${project.links.view}" class="btn" target="_blank" rel="noopener noreferrer">
+            <i class="fas fa-eye"></i> View
+          </a>
         </div>
       </div>
     </div>`
