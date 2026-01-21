96c0ecb6cca4a6637b0c9f254acb8d548016adb3
Panda-webai <viraput.c@gmail.com>
2026-01-21 17:29:42 +0900

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Viraput Chaupratoom";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
@@ -136,6 +136,7 @@ document.onkeydown = function (e) {
     }
 
 }
+
 
 
 
